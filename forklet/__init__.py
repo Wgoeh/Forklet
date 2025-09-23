064718636899260e8678563e566b0c211d2abc66
#Einswilli <61468993+Einswilli@users.noreply.github.com>
2025-09-23 10:53:52 +0200

    feat (api): Add a Python API interface. (#12)

--- forklet/__init__.py
+++ forklet/__init__.py
@@ -9,7 +9,7 @@ __version__ = "0.1.0"
 __author__ = "AllDotPy"
 __description__ = "Download any file or folder from any GitHub repo by branch, tag, or commit with glob pattern filtering."
 
-# from forklet.interfaces.api import GitHubDownloader
+from forklet.interfaces.api import GitHubDownloader
 from forklet.models import (
     DownloadRequest, DownloadResult, DownloadStrategy, FilterCriteria,
     RepositoryInfo, GitReference, ProgressInfo, DownloadStatus
